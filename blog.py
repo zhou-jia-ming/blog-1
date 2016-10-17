6222dfbd04f3396ee4d3b07df33dc37f059b343c
zhoujiaming <zhoujiaming@imbaimei.com>
2016-10-17 23:21:12 +0800

    修改端口号

--- blog.py
+++ blog.py
@@ -79,7 +79,7 @@ app.register_blueprint(api, url_prefix="/api")
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=5000,
+        port=8080,
         debug=False,
     )
     pass
